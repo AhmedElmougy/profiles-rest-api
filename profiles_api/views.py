63bae70f7ba79e515303e66512218cfdee28b80e
AhmedElmougy <ahmed.m.elmogy@gmail.com>
2021-02-18 17:32:26 +0200

    added user search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
+
 
 from profiles_api.serializers import UserSerializer
 from profiles_api.permissions import UpdateOwnProfile
@@ -14,3 +16,5 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     Authentication_classes = [TokenAuthentication,]
     permission_classes = [permissions.IsAuthenticated,UpdateOwnProfile,]
+    filter_backends = [filters.SearchFilter,]
+    search_fields  = ['username','email'] 
